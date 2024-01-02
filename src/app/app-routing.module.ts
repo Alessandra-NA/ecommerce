c0d677e4a402636541cd4f1e9c2e0e2620163006
Alessandra Nuñez <alessandra.na001@gmail.com>
2024-01-01 19:23:14 -0500

    initial setup
    added modules:
    - cart
    - home
    - product
    - user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/user/components/login/login.component';
+import { SignupComponent } from './modules/user/components/signup/signup.component';
+import { HomeComponent } from './modules/home/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '**', redirectTo: '', },
+   { path: '', component:  HomeComponent},
+   { path: 'login', component: LoginComponent },
+   { path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserModule } from './modules/user/user.module';
+import { CartModule } from './modules/cart/cart.module';
+import { ProductModule } from './modules/product/product.module';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      UserModule,
+      CartModule,
+      ProductModule,
+      HomeModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
